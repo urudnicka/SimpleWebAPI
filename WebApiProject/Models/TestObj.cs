02fa2b07c2bebb787b6abcfd91fab6e90cc02cef
Urszula Rudnicka <urudnicka400@gmail.com>
2021-07-01 02:04:15 +0200

    Made Number nullable

--- WebApiProject/Models/TestObj.cs
+++ WebApiProject/Models/TestObj.cs
@@ -8,11 +8,11 @@ namespace WebApiProject.Models
 {
     public class TestObj
     {
-        [Required]
+        [Required(ErrorMessage = "Number field is required")]
         [Range(0,10, ErrorMessage = "Number must be between 0 and 10")]
-        public int Number { get; set; }
+        public int? Number { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Text field is required")]
         [RegularExpression("[abcd]+", ErrorMessage = "Text may contain only letters [a,b,c,d]")]
         public string Text { get; set; }
     }
